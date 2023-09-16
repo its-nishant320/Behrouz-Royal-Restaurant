4339b927c79026852a96f6069ea8162e7b072b29
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-09-17 02:43:51 +0530

    4

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,32 @@ const addEventOnElements = function (elements, eventType, callback) {
   }
   
   addEventOnElements(navTogglers, "click", toggleNavbar);
+
+//header
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function () {
+  const isScrollBottom = lastScrollPos < window.scrollY;
+  if (isScrollBottom) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScrollPos = window.scrollY;
+}
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+    hideHeader();
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+

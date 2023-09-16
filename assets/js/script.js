160ab6affd8533a5042c2deb716b200588e1ef1d
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-09-16 19:08:59 +0530

    3

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,22 @@ window.addEventListener("load",function(){
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 });
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+      elements[i].addEventListener(eventType, callback);
+    }
+  }
+  
+  
+  const navbar = document.querySelector("[data-navbar]");
+  const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+  const overlay = document.querySelector("[data-overlay]");
+  
+  const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+  }
+  
+  addEventOnElements(navTogglers, "click", toggleNavbar);

f3cd5422ca80d9de774bcd30f99f58320d99a2a6
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-10-25 11:34:08 +0530

    script file

--- script.js
+++ script.js
@@ -28,18 +28,18 @@ window.addEventListener("scroll", function () {
 });
 
 
-// const preloader = document.querySelector(".data-preaload");
+const preloader = document.querySelector(".data-preaload");
 
-// window.addEventListener("load",function(){
-//     preloader.classList.add("loaded");
-//     document.body.classList.add("loaded");
-// });
+window.addEventListener("load",function(){
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+});
 
-// const addEventOnElements = function (elements, eventType, callback) {
-//     for (let i = 0, len = elements.length; i < len; i++) {
-//       elements[i].addEventListener(eventType, callback);
-//     }
-//   }
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+      elements[i].addEventListener(eventType, callback);
+    }
+  }
   
   
   const navbar = document.querySelector("[data-navbar]");
